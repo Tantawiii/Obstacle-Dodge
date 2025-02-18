1351317219acc93741165450def8228ad5f0d8ed
Omar Tantawy <o.tantawy.o@gmail.com>
2025-02-18 08:24:33 +0200

    Command Pattern
    Done fully but movement is kinda fun but weird but its fun and makes the game more difficult and each one is a different action or command to say that runs the code smoothly and uses less update cases for better optimization!

--- Assets/Scripts/Command.cs
+++ Assets/Scripts/Command.cs
@@ -2,21 +2,66 @@ using UnityEngine;
 
 public abstract class Command
 {
-    public abstract void Execute(Animator anim);
+    public abstract void Execute(Animator anim, bool correct);
 }
 
 public class MoveForward : Command
 {
-    public override void Execute(Animator anim)
+    public override void Execute(Animator anim, bool correct)
     {
-        anim.SetTrigger("Walking");
+        if(correct){
+            anim.SetTrigger("Walking");
+        }
+        else
+        {
+            anim.SetTrigger("Backing");
+        }
+    }
+}
+
+public class MoveBackward : Command
+{
+    public override void Execute(Animator anim, bool correct)
+    {
+        if(correct){
+            anim.SetTrigger("Backing"); 
+        }
+        else
+        {
+            anim.SetTrigger("Walking");
+        }
+    }
+}
+
+public class MoveRightward : Command
+{
+    public override void Execute(Animator anim, bool correct)
+    {
+        if(correct){
+            anim.SetTrigger("GoingRight");
+        }
+        else{
+            anim.SetTrigger("GoingLeft");
+        }
+    }
+}
+
+public class MoveLeftward : Command
+{
+    public override void Execute(Animator anim, bool correct)
+    {
+        if(correct){
+            anim.SetTrigger("GoingLeft");
+        }
+        else{
+            anim.SetTrigger("GoingRight");
+        }
     }
 }
 
 public class DoNothing : Command
 {
-    public override void Execute(Animator anim)
+    public override void Execute(Animator anim, bool correct)
     {
-
     }
 }
--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -1,18 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
 using Unity.Cinemachine;
 using UnityEngine;
 
 public class InputHandler : MonoBehaviour
 {
     [SerializeField] GameObject ourPlayer;
-    Command move;
+    Command move, back, right, left;
     CinemachineCamera cinemachineCam;
     Animator anim;
+    List<Command> oldCommands = new List<Command>();
+    Coroutine replayCoroutine;
+    bool shouldStartReplay, isReplaying;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [System.Obsolete]
     void Start()
     {
         move = new MoveForward();
+        back = new MoveBackward();
+        right = new MoveRightward();
+        left = new MoveLeftward();
         anim = ourPlayer.GetComponent<Animator>();
         cinemachineCam = FindObjectOfType<CinemachineCamera>();
         if (cinemachineCam != null && ourPlayer != null)
@@ -24,8 +32,66 @@ public class InputHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)){
-            move.Execute(anim);
+        if(!isReplaying){
+            HandleInput();
         }
+        StartReplay();
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            move.Execute(anim, true);
+            oldCommands.Add(move);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            back.Execute(anim, true);
+            oldCommands.Add(move);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            right.Execute(anim, true);
+            oldCommands.Add(move);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            left.Execute(anim, true);
+            oldCommands.Add(move);
+        }
+        if (Input.GetKeyDown(KeyCode.Space)){
+            shouldStartReplay = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Z)){
+            UndoLastCommand();
+        }
+    }
+
+    void UndoLastCommand(){
+        if(oldCommands.Count > 0){
+            Command c = oldCommands[oldCommands.Count - 1];
+            c.Execute(anim, false);
+            oldCommands.RemoveAt(oldCommands.Count - 1);
+        }
+    }
+
+    void StartReplay(){
+        if(shouldStartReplay && oldCommands.Count > 0){
+            shouldStartReplay = false;
+            if(replayCoroutine != null){
+                StopCoroutine(replayCoroutine);
+            }
+            replayCoroutine = StartCoroutine(ReplayCommands());
+        }
+    }
+
+    IEnumerator ReplayCommands(){
+        isReplaying = true;
+        for (int i = 0; i < oldCommands.Count; i++){
+            oldCommands[i].Execute(anim, true);
+            yield return new WaitForSeconds(1.0f);
+        }
+        isReplaying = false;
     }
 }
